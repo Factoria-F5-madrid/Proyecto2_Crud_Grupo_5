ccc6217315ba78bfaf37fddfd578ec6c23470aa4
johiortiz <ortizvallejosjohi@gmail.com>
2025-07-19 16:04:44 +0200

    feat(serializers): serializers created

--- fenix/prendas/serializers.py
+++ fenix/prendas/serializers.py
@@ -1,4 +1,8 @@
 from rest_framework import serializers
-from .models import Prendas
+from .models import Prenda
 
-
+class PrendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prenda
+        fields = '__all__'
+        
