f1e64f89e2bda38eb6b58d6ae477732a8fdc1ff9
johiortiz <ortizvallejosjohi@gmail.com>
2025-07-28 23:35:23 +0200

    fix: wrong proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://fenix-pbad.onrender.com/api'
     }
   },
   // Añade esta sección para resolver las rutas con @
